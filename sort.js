80a074bfcefbb151499f6dbe3ddd67f788b6f690
BikashPanda1998 <bikashpanda342@gmail.com>
2023-07-01 08:55:30 +0530

    first commit

---
+++ sort.js
@@ -0,0 +1,15 @@
+    let sopt=document.getElementById('select');
+    let images=document.querySelectorAll('.gallery .product');
+    sopt.onchange=()=>{
+        var svalue=sopt.value;
+        //console.log(svalue);
+        if(svalue==="asc"){
+            
+        }
+        images.forEach(filter=>{
+            let name=filter.getAttribute('data-name');
+        
+        });
+    };
+  
+   
